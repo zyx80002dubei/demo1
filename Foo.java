cb9750870360397db504a6930ae2e6046d3fca97
zyx80002dubei <zyx80002@163.com>
2018-12-11 20:54:45 +0800

    学习github
    学习github!!!

---
+++ Foo.java
@@ -0,0 +1,29 @@
+package com.it.hrhz;
+/**
+ * 
+ * 
+ * 
+ * 
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:com.it.hrhz.Foo.java
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2016-4-12
+ * </p>
+ * <p>
+ * Company: 中软创新(北京)科技有限公司
+ * 
+ * 
+ * </p>
+ * 
+ * @author 郑向云
+ */
+public class Foo {
+	public static void main(String[] args) {
+		System.out.println("我是一个包的类Foo");
+	}
+
+}
